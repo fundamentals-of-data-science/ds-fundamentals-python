874ce0ab92af1aa0faaf0caacaa864ee48f19521
Andrew Stewart <andrew.c.stewart@gmail.com>
2019-04-30 22:34:49 -0700

    Bump version: 0.1.3 → 0.1.4

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/fundamentals-of-data-science/ds-fundamentals-python',
-    version='0.1.3',
+    version='0.1.4',
     zip_safe=False,
 )
