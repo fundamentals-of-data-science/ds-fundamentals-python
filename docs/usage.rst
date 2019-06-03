01797e926efb9712930c1fea680a09c348c23e4d
Andrew Stewart <andrew.c.stewart@gmail.com>
2019-06-03 14:17:28 -0700

    added core dependencies

--- docs/usage.rst
+++ docs/usage.rst
@@ -4,4 +4,4 @@ Usage
 
 To use Fundamentals of Data Science in a project::
 
-    import fundamentals_of_data_science
+    import fundamentals_of_data_science as fds
--- setup.py
+++ setup.py
@@ -11,7 +11,16 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = [ 
+    'numpy', 
+    'scipy', 
+    'pandas', 
+    'matplotlib',
+    'seaborn',
+    'statsmodels',
+    'sklearn',
+    'patsy'
+    ]
 
 setup_requirements = ['pytest-runner', ]
 
